e7712123b23135cb432dcfa3c5402bbb686d7114
Florian Luetticke <16699742+FlorianLuetticke@users.noreply.github.com>
2023-04-15 23:48:42 +0200

    WIP

--- src/math/vec4.h
+++ src/math/vec4.h
@@ -1,16 +1,46 @@
 #include <array>
+#include <stdexcept>
 
 class TVec4 {
+    std::array<double, 4> _data;
 
-  std::array<double, 4> _data;
+   public:
+    TVec4() { _data.fill(0.); }
+    TVec4(const std::array<double, 4> &l) : _data(l) {}
+    TVec4(const std::array<double, 3> &l) {
+        _data[0] = l[0];
+        _data[1] = l[1];
+        _data[2] = l[2];
+        _data[3] = 0;
+    }
 
-private:
-  TVec4() { _data.fill(0.); }
-  TVec4(const std::array<double, 4> &l) : _data(l) {}
-  TVec4(const std::array<double, 3> &l) {
-    _data[0] = l[0];
-    _data[1] = l[1];
-    _data[2] = l[2];
-    _data[3] = 0;
-  }
+    TVec4(double x, double y, double z, double t = 0.) : _data{x, y, z, t} {}
+
+    double &operator[](size_t i) { return _data[i]; }
+
+    TVec4 &operator+=(const TVec4 &other) {
+        for (size_t i = 0; i < 4; ++i) {
+            _data[i] += other._data[i];
+        }
+        return *this;
+    }
+
+    TVec4 &operator-=(const TVec4 &other) {
+        for (size_t i = 0; i < 4; ++i) {
+            _data[i] -= other._data[i];
+        }
+        return *this;
+    }
+
+    TVec4 operator+(const TVec4 &other) {
+        TVec4 ret = *this;
+        ret += other;
+        return ret;
+    }
+
+    TVec4 operator-(const TVec4 &other) {
+        TVec4 ret = *this;
+        ret -= other;
+        return ret;
+    }
 };
--- test/mathTest/src/vecTests.cpp
+++ test/mathTest/src/vecTests.cpp
@@ -1,7 +1,77 @@
-#include "vec4.h"
 #include "gtest/gtest.h"
+#include "vec4.h"
 
 TEST(TVec4, Create) {
-  TVec4 vec;
-  SUCCEED();
+    TVec4 vec1;
+
+    TVec4 vec2 = {1., 2., 3.};
+    TVec4 vec3 = {1., 2., 3., 4.};
+
+    SUCCEED();
+}
+
+TEST(TVec4, DefaultValues) {
+    TVec4 vec;
+
+    ASSERT_EQ(vec[0], 0.);
+    ASSERT_EQ(vec[1], 0.);
+    ASSERT_EQ(vec[2], 0.);
+    ASSERT_EQ(vec[3], 0.);
+}
+
+TEST(TVec4, FourthValueDefaultsToZero) {
+    TVec4 vec = {1., 2., 3.};
+
+    ASSERT_EQ(vec[0], 1.);
+    ASSERT_EQ(vec[1], 2.);
+    ASSERT_EQ(vec[2], 3.);
+    ASSERT_EQ(vec[3], 0.);
+}
+
+TEST(TVec4, GetValues) {
+    TVec4 vec = {1., 2., 3., 5.};
+
+    ASSERT_EQ(vec[0], 1.);
+    ASSERT_EQ(vec[1], 2.);
+    ASSERT_EQ(vec[2], 3.);
+    ASSERT_EQ(vec[3], 5.);
+}
+
+TEST(TVec4, AddToVector) {
+    TVec4 vec = {1., 2., 3., 5.};
+    TVec4 vec2 = {2., 3., -9., 0.25};
+    vec += vec2;
+    ASSERT_EQ(vec[0], 3.);
+    ASSERT_EQ(vec[1], 5.);
+    ASSERT_EQ(vec[2], -6.);
+    ASSERT_EQ(vec[3], 5.25);
+}
+
+TEST(TVec4, AddVectors) {
+    TVec4 vec = {1., 2., 3., 5.};
+    TVec4 vec2 = {2., 3., -9., 0.25};
+    auto vec3 = vec2 + vec;
+    ASSERT_EQ(vec3[0], 3.);
+    ASSERT_EQ(vec3[1], 5.);
+    ASSERT_EQ(vec3[2], -6.);
+    ASSERT_EQ(vec3[3], 5.25);
+}
+
+TEST(TVec4, SubtractFromVector) {
+    TVec4 vec = {1., 2., 3., 5.};
+    TVec4 vec2 = {2., 3., -9., 0.25};
+    auto vec3 = vec2 - vec;
+    ASSERT_EQ(vec3[0], 1.);
+    ASSERT_EQ(vec3[1], 1.);
+    ASSERT_EQ(vec3[2], -12.);
+    ASSERT_EQ(vec3[3], -4.75);
+}
+
+TEST(TVec4, ScalarMultipyVectors) {
+    TVec4 vec = {1., 2., 3., 5.};
+    vec *= 2;
+    ASSERT_EQ(vec[0], 2.);
+    ASSERT_EQ(vec[1], 4.);
+    ASSERT_EQ(vec[2], 6.);
+    ASSERT_EQ(vec[3], 10.);
 }
