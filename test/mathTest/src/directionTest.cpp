27970fc996fb45db9227ed800a99cc3771600a91
Florian Luetticke <16699742+FlorianLuetticke@users.noreply.github.com>
2023-04-17 23:00:16 +0200

    add tests for dot/norm/cross

--- test/mathTest/src/directionTest.cpp
+++ test/mathTest/src/directionTest.cpp
@@ -101,3 +101,41 @@ TEST(TDirection, scalarDivision) {
     ASSERT_EQ(direction, TDirection(-10., -12., -6.));
     ASSERT_EQ(result, TDirection(2.5, 3., 1.5));
 }
+
+TEST(TDirection, Norm) {
+    TDirection direction = {2., 4., 4.};
+    ASSERT_EQ(direction.norm(), 6.);
+}
+
+TEST(TDirection, DotProduct) {
+    TDirection dir = {1., 2., 3.};
+    TDirection dir2 = {2., 3., -9.};
+    auto dot = dir.dot(dir2);
+    auto dot2 = dir2.dot(dir);
+    ASSERT_EQ(dot, dot2);
+    ASSERT_EQ(dot, 2. + 6. - 27.);
+}
+
+TEST(TDirection, CrossProduct) {
+    TDirection dir1 = {1., 2., 0.};
+    TDirection dir2 = {2., -1., 0.};
+    TDirection dir3 = {0., 0, 3.};
+    ASSERT_EQ(dir1.dot(dir2), 0.);
+    ASSERT_EQ(dir1.dot(dir3), 0.);
+    ASSERT_EQ(dir2.dot(dir3), 0.);
+
+    auto cross1 = dir1.cross(dir2);
+    ASSERT_EQ(cross1.dot(dir1), 0.);
+    ASSERT_EQ(cross1.dot(dir2), 0.);
+    ASSERT_DOUBLE_EQ(cross1.norm(), dir1.norm() * dir2.norm());
+
+    cross1 = dir2.cross(dir3);
+    ASSERT_EQ(cross1.dot(dir2), 0.);
+    ASSERT_EQ(cross1.dot(dir3), 0.);
+    ASSERT_EQ(cross1.norm(), dir2.norm() * dir3.norm());
+
+    cross1 = dir1.cross(dir3);
+    ASSERT_EQ(cross1.dot(dir1), 0.);
+    ASSERT_EQ(cross1.dot(dir3), 0.);
+    ASSERT_EQ(cross1.norm(), dir1.norm() * dir3.norm());
+}
