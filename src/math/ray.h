71fe8f4a09fd54409327d217c4620689da92a0ed
Florian Luetticke <16699742+FlorianLuetticke@users.noreply.github.com>
2023-04-24 00:05:59 +0200

    refractor ray

--- src/math/ray.h
+++ src/math/ray.h
@@ -9,22 +9,25 @@
 #include "vector.h"
 
 class TRay {
-    TPoint origin;
-    TVector direction;
+    TPoint _origin;
+    TVector _direction;
 
    public:
     TRay(){};
-    TRay(const TPoint &origin_, const TVector &direction_)
-        : origin(origin_), direction(direction_){};
+    TRay(const TPoint &origin, const TVector &direction)
+        : _origin(origin), _direction(direction){};
 
     bool operator==(const TRay &other) const {
-        return origin == other.origin && direction == other.direction;
+        return _origin == other._origin && _direction == other._direction;
     }
     bool operator!=(const TRay &other) const { return not(*this == other); }
 
-    TPoint at(double x) const { return origin + x * direction; }
+    TPoint at(double x) const { return _origin + x * _direction; }
+
+    const TPoint &origin() const { return _origin; }
+    const TVector &direction() const { return _direction; }
     friend std::ostream &operator<<(std::ostream &os, const TRay &tr) {
-        os << tr.origin << " +x*" << tr.direction;
+        os << tr._origin << " +x*" << tr._direction;
         return os;
     }
 };
