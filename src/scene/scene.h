43e9c21d6167a8204fb867147bd0771eebf5bb6d
Florian Luetticke <16699742+FlorianLuetticke@users.noreply.github.com>
2023-04-26 00:24:55 +0200

    added scene

---
+++ src/scene/scene.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <limits>
+#include <vector>
+
+#include "math/ray.h"
+#include "primitives/sphere.h"
+
+// camera points towards X for now.
+class TScene {
+    std::vector<TSphere> spheres;
+
+   public:
+    TScene(){};
+
+    void add(const TSphere& in) { spheres.push_back(in); }
+
+    std::optional<sHitInfo> findHit(const TRay& ray) const {
+        double distance = std::numeric_limits<double>::max();
+        std::optional<sHitInfo> ret;
+        for (const auto& sphere : spheres) {
+            auto optHit = sphere.intersect(ray);
+            if (optHit) {
+                double thisDistance =
+                    ray.origin().distanceSquaredTo(optHit->intersection);
+
+                if (thisDistance < distance) {
+                    ret = optHit;
+                    distance = thisDistance;
+                }
+            }
+        }
+        return ret;
+    }
+};
