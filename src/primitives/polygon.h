7d8c1c972baaed1591f163ee15ed1637c057d013
Florian Luetticke <16699742+FlorianLuetticke@users.noreply.github.com>
2023-05-03 00:33:22 +0200

    wip test for polygon hit

--- src/primitives/polygon.h
+++ src/primitives/polygon.h
@@ -11,11 +11,30 @@
 #include "math/transformation.h"
 
 class TPolygon {
+    TPoint _a;
+    TPoint _b;
+    TPoint _c;
+
    public:
     TPolygon(){};
 
-    TPolygon(const TPoint& a, const TPoint& b, const TPoint& c){};
+    TPolygon(const TPoint& a, const TPoint& b, const TPoint& c) : _a(a), _b(b), _c(c){};
 
-    TVector u() { return {1, 0, 0}; }
-    TVector v() { return {0, 1, 0}; }
+    TVector u() { return _b - _a; }
+    TVector v() { return _c - _a; }
+
+    TVector normal() { return u().cross(v()).normed(); }
+
+    std::optional<sHitInfo> intersect(const TRay& ray) const {
+        // create a  new base where u  maps to 1,0,0;
+        // v maps to 0,1,0; and u x v maps to z.
+        // and the point  a transforms to Point 0,0,0
+
+        // Then  try to hit the (z=0) plane. The hit is in the triangle if (x>=0, y>=0 and x +
+        // y<=1;)
+
+        TTransformation trafo;
+
+        return {};
+    }
 };
--- test/mathTest/src/polygonTest.cpp
+++ test/mathTest/src/polygonTest.cpp
@@ -32,3 +32,34 @@ TEST(TPolygon, UVdependOnInput) {
     ASSERT_EQ(poly.u(), TVector(0, 1, 1));
     ASSERT_EQ(poly.v(), TVector(0, -1, 0));
 }
+
+TEST(TPolygon, AccessNormal) {
+    TPolygon poly{TPoint(2, 0, 0), TPoint(3, 0, 0), TPoint(2, 1, 0)};
+
+    ASSERT_EQ(poly.normal(), TVector(1, 0, 0).cross(TVector(0, 1, 0)));
+}
+
+TEST(TPolygon, intersectMiss) {
+    TPolygon poly{TPoint(2, 0, 0), TPoint(3, 0, 0), TPoint(2, 1, 0)};
+
+    ASSERT_FALSE(poly.intersect(TRay(TPoint(1.9, 0.1, -1), TVector(0, 0, 1))));
+    ASSERT_FALSE(poly.intersect(TRay(TPoint(2.1, -0.1, -1), TVector(0, 0, 1))));
+
+    ASSERT_FALSE(poly.intersect(TRay(TPoint(3.1, 0.01, -1), TVector(0, 0, 1))));
+    ASSERT_FALSE(poly.intersect(TRay(TPoint(2.9, -0.01, -1), TVector(0, 0, 1))));
+
+    ASSERT_FALSE(poly.intersect(TRay(TPoint(1.9, 0.99, -1), TVector(0, 0, 1))));
+    ASSERT_FALSE(poly.intersect(TRay(TPoint(2.01, 1.01, -1), TVector(0, 0, 1))));
+
+    ASSERT_FALSE(poly.intersect(TRay(TPoint(2.51, 0.51, -1), TVector(0, 0, 1))));
+    ASSERT_FALSE(poly.intersect(TRay(TPoint(2.51, -0.01, -1), TVector(0, 0, 1))));
+    ASSERT_FALSE(poly.intersect(TRay(TPoint(1.99, 0.51, -1), TVector(0, 0, 1))));
+}
+
+TEST(TPolygon, intersectHit) {
+    TPolygon poly{TPoint(2, 0, 0), TPoint(3, 0, 0), TPoint(2, 1, 0)};
+
+    ASSERT_TRUE(poly.intersect(TRay(TPoint(2.49, 0.49, -1), TVector(0, 0, 1))));
+    ASSERT_TRUE(poly.intersect(TRay(TPoint(2.51, 0.01, -1), TVector(0, 0, 1))));
+    ASSERT_TRUE(poly.intersect(TRay(TPoint(2.01, 0.51, -1), TVector(0, 0, 1))));
+}
