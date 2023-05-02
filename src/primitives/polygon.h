0d19e98d099a1b30a4cc87ac82675bb8ce982ff3
Florian Luetticke <16699742+FlorianLuetticke@users.noreply.github.com>
2023-05-02 22:56:24 +0200

    added  polygon stub

---
+++ src/primitives/polygon.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <array>
+#include <cmath>
+#include <iostream>
+#include <optional>
+#include <ostream>
+#include <stdexcept>
+
+#include "hitInfo.h"
+#include "math/ray.h"
+#include "math/transformation.h"
+
+class TPolygon {
+   public:
+    TPolygon(){};
+};
---
+++ test/mathTest/src/polygonTest.cpp
@@ -0,0 +1,15 @@
+
+
+#include <cmath>
+
+#include "gtest/gtest.h"
+#include "math/point.h"
+#include "math/vector.h"
+#include "primitives/polygon.h"
+
+TEST(TPolygon, Create) {
+    TPolygon poly;
+
+    // TPolygon sphere2{3.4};
+    SUCCEED();
+}
