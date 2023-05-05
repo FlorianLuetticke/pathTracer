e596d05c00e8b7d8dd2cb1a2c3c88798d357fafa
Florian Luetticke <16699742+FlorianLuetticke@users.noreply.github.com>
2023-05-05 23:26:26 +0200

    polygon hit

--- src/primitives/polygon.h
+++ src/primitives/polygon.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <array>
 #include <cmath>
+#include <iomanip>
 #include <iostream>
 #include <optional>
 #include <ostream>
@@ -9,21 +10,32 @@
 #include "hitInfo.h"
 #include "math/ray.h"
 #include "math/transformation.h"
+#include "math/vector.h"
 
 class TPolygon {
-    TPoint _a;
-    TPoint _b;
-    TPoint _c;
+    TPoint _origin;
+    TVector _u;
+    TVector _v;
+    TVector _normal;
+
+    TTransformation _toUV;
 
    public:
     TPolygon(){};
 
-    TPolygon(const TPoint& a, const TPoint& b, const TPoint& c) : _a(a), _b(b), _c(c){};
+    TPolygon(const TPoint& a, const TPoint& b, const TPoint& c)
+        : _origin(a),
+          _u(b - a),
+          _v(c - a),
+          _normal(_u.cross(_v).normed()),
+          _toUV(TTransformation::BaseChangeWithTranslation(_u, _v, _normal, _origin)){
 
-    TVector u() { return _b - _a; }
-    TVector v() { return _c - _a; }
+          };
 
-    TVector normal() { return u().cross(v()).normed(); }
+    TVector u() { return _u; }
+    TVector v() { return _v; }
+
+    TVector normal() { return _normal; }
 
     std::optional<sHitInfo> intersect(const TRay& ray) const {
         // create a  new base where u  maps to 1,0,0;
@@ -33,7 +45,53 @@ class TPolygon {
         // Then  try to hit the (z=0) plane. The hit is in the triangle if (x>=0, y>=0 and x +
         // y<=1;)
 
-        TTransformation trafo;
+        // std::cout << "ray " << ray << std::endl;
+
+        auto uvRay = _toUV.transform(ray);
+
+        auto directionInNormal = uvRay.direction().z();
+
+        // std::cout << "uvray " << uvRay << std::endl;
+        //  parallel to polygon -> no hit.
+        if (std::abs(directionInNormal) < 1e-10) {
+            std::cout << " parallel to polygon" << std::endl;
+            return {};
+        }
+        auto hitInZUnits = -uvRay.origin().z() / directionInNormal;
+        // intersection before ray origin;
+        if (hitInZUnits < 0) {
+            std::cout << "Hit before start" << std::endl;
+            return {};
+        }
+        TPoint intersection = uvRay.at(hitInZUnits);
+        double u = intersection.x();
+        double v = intersection.y();
+        // std::cout << "intersection " << intersection << std::endl;
+        if (u > 0 && v > 0 && u + v <= 1) {
+            // TPoint test = toUV.inverseTransform(intersection);
+            //  TPoint reverse = ray.at(hitInZUnits);
+
+            // if (reverse != test) {
+            //     std::cout << "Error with logic!" << test << " vs " << reverse << " \n";
+            // }
+
+            sHitInfo ret;
+            ret.intersection = ray.at(hitInZUnits);
+            // auto normalTest = toUV.inverseTransform(TVector(0, 0, 1));
+            //  TVector normal{toUV.getInverseTransposedTrafoMatrix()(2, 0),
+            //                 toUV.getInverseTransposedTrafoMatrix()(2, 2)};
+            //                 toUV.getInverseTransposedTrafoMatrix()(2, 1),
+
+            // if (normal != normalTest) {
+            //     std::cout << "Error with normal logic!" << test << " vs " << reverse << " \n";
+            // }
+            // if (std::abs(normal.norm() - 1.) > 1e-14) {
+            //     std::cout << "Error with normal norm!" << normal.norm()
+            //               << " (diff: " << std::setprecision(15) << normal.norm() - 1. << ") \n";
+            // }
+            ret.normal = _normal;
+            return ret;
+        }
 
         return {};
     }
--- test/mathTest/src/polygonTest.cpp
+++ test/mathTest/src/polygonTest.cpp
@@ -4,6 +4,7 @@
 
 #include "gtest/gtest.h"
 #include "math/point.h"
+#include "math/transformation.h"
 #include "math/vector.h"
 #include "primitives/polygon.h"
 
@@ -59,7 +60,41 @@ TEST(TPolygon, intersectMiss) {
 TEST(TPolygon, intersectHit) {
     TPolygon poly{TPoint(2, 0, 0), TPoint(3, 0, 0), TPoint(2, 1, 0)};
 
-    ASSERT_TRUE(poly.intersect(TRay(TPoint(2.49, 0.49, -1), TVector(0, 0, 1))));
+    auto intersection = poly.intersect(TRay(TPoint(2.49, 0.49, -1), TVector(0, 0, 1)));
+    ASSERT_TRUE(intersection);
+    ASSERT_EQ(intersection->normal, TVector(0, 0, 1));
+    ASSERT_EQ(intersection->intersection, TPoint(2.49, 0.49, 0));
     ASSERT_TRUE(poly.intersect(TRay(TPoint(2.51, 0.01, -1), TVector(0, 0, 1))));
     ASSERT_TRUE(poly.intersect(TRay(TPoint(2.01, 0.51, -1), TVector(0, 0, 1))));
+
+    TPolygon poly2{TPoint(0, 2, 0), TPoint(0, 3, 0), TPoint(0, 2, 1)};
+
+    ASSERT_TRUE(poly2.intersect(TRay(TPoint(1, 2.49, 0.49), TVector(-1, 0, 0))));
+    ASSERT_TRUE(poly2.intersect(TRay(TPoint(1, 2.51, 0.01), TVector(-1, 0, 0))));
+    ASSERT_TRUE(poly2.intersect(TRay(TPoint(1, 2.01, 0.51), TVector(-1, 0, 0))));
+
+    for (double alpha = 0.1; alpha < 3.11; alpha += 0.1) {
+        for (double beta = 0.1; beta < 3.11; beta += 0.1) {
+            for (double gamma = 0.1; gamma < 3.11; gamma += 0.1) {
+                for (double offset = 0.1; offset < 3.11; offset += 0.1) {
+                    auto preRotate = TTransformation::EulerRotate(
+                        alpha, beta, gamma,
+                        TPoint(-11 * offset, 0.17 - offset, -1.26 * offset * offset));
+
+                    TPolygon poly3{preRotate.transform(TPoint(0, 2, 0)),
+                                   preRotate.transform(TPoint(0, 3, 0)),
+                                   preRotate.transform(TPoint(0, 2, 1))};
+                    auto testRay =
+                        preRotate.transform(TRay(TPoint(1, 2.49, 0.49), TVector(-1, 0, 0)));
+
+                    auto intersection3 = poly3.intersect(testRay);
+
+                    ASSERT_TRUE(intersection3);
+                    ASSERT_EQ(intersection3->normal, preRotate.transform(TVector(1, 0, 0)));
+                    ASSERT_EQ(intersection3->intersection,
+                              preRotate.transform(TPoint(0, 2.49, 0.49)));
+                }
+            }
+        }
+    }
 }
